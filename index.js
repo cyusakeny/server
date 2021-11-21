69d578bcbe0cb81151caa281e71eb8aa66a65ff7
Cyusa Munezero Keny <gasananike@gmail.com>
2021-11-21 18:07:04 +0200

    continuation

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-
 io.on('connection',socket =>{
-    socket.on('textmessage',({message,name})=>{
-        if (socket.broadcast.emit('message',{message,name})) {
-            console.log("Message sent: "+message)
-            console.log("Sent By: "+name)        
-        }
-        
-        
-    } )
+    socket.on('JoinChatRoom',(UserName,RoomName)=>{
+        console.log("User:"+UserName)
+        console.log("Room:"+RoomName)
+       socket.broadcast.emit("GetMyUser",UserName,RoomName,socket.id)  
+    });
 })
-http.listen(4000,function (){
-    console.log("Server listening on port 4000")
+http.listen(1650,function (){
+    console.log("Server listening on port 5000")
 })
